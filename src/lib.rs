e98ea84cd839275d3d77e0b6147976d99b859bc5
samarth <samarthkulkarni@icloud.com>
2025-01-27 00:12:59 +0530

    kal kar lunga

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,8 @@ pub enum ExpansionBuildError {
 #[derive(Debug)]
 pub enum QueryError {
     ExpansionDNE,
-    MissingParentheses
+    MissingParentheses,
+    IncorrectNumberOfArguments
 }
 
 #[derive(Debug)]
@@ -21,7 +22,7 @@ pub struct Expansion {
     name: String,
     args: Vec<String>,
     body: Vec<Token>,
-    replace_map: HashMap<String, Vec<usize>>
+    replace_map: Vec<Vec<usize>>
 }
 
 impl Expansion {
@@ -43,12 +44,12 @@ impl Expansion {
         }
 
         if !matches!(expansion_build_tokens[3], Token::OpenParentheses) { return Err(ExpansionBuildError::MissingParentheses); }
-        let mut i = 4;
+        let mut idx = 4;
 
         loop {
-            if i+2 >= expansion_build_tokens.len() { return Err(ExpansionBuildError::MissingClosingParentheses); }
-            if matches!(expansion_build_tokens[i], Token::Dollar) {
-                if let Token::Literal(expansion_arg_str) = &expansion_build_tokens[i+1] {
+            if idx+2 >= expansion_build_tokens.len() { return Err(ExpansionBuildError::MissingClosingParentheses); }
+            if matches!(expansion_build_tokens[idx], Token::Dollar) {
+                if let Token::Literal(expansion_arg_str) = &expansion_build_tokens[idx+1] {
                     args.push(expansion_arg_str.clone());
                 } else {
                     return Err(ExpansionBuildError::MissingName);
@@ -56,38 +57,35 @@ impl Expansion {
             } else {
                 return Err(ExpansionBuildError::MissingDollar);
             }
-            if matches!(expansion_build_tokens[i+2], Token::Comma) {
-                if matches!(expansion_build_tokens[i+3], Token::CloseParentheses) {
-                    i += 4;
+            if matches!(expansion_build_tokens[idx+2], Token::Comma) {
+                if matches!(expansion_build_tokens[idx+3], Token::CloseParentheses) {
+                    idx += 4;
                     break;
                 }
-                i += 3;
-            } else if matches!(expansion_build_tokens[i+2], Token::CloseParentheses) {
-                i += 3;
+                idx += 3;
+            } else if matches!(expansion_build_tokens[idx+2], Token::CloseParentheses) {
+                idx += 3;
                 break;
             } else {
                 return Err(ExpansionBuildError::MissingClosingParentheses);
             }
         }
         let mut body = Vec::new();
-        if matches!(expansion_build_tokens[i+1], Token::OpenParentheses) {
-            body = expansion_build_tokens[i+1..expansion_build_tokens.len()-3].to_vec();
+        if matches!(expansion_build_tokens[idx+1], Token::OpenParentheses) {
+            body = expansion_build_tokens[idx+1..expansion_build_tokens.len()-3].to_vec();
         }
 
-        let mut replace_map: HashMap<String, Vec<usize>> = HashMap::new();
+        let mut replace_map: Vec<Vec<usize>> = Vec::with_capacity(args.len());
         let mut body_idx = 1;
         while body_idx < body.len() {
             if !matches!(body[body_idx], Token::Literal(_)) { 
                 body_idx += 1;
                 continue;
             }
-            for arg in &args {
+            for (arg_idx, arg) in args.iter().enumerate() {
                 if let Token::Literal(body_literal) = &body[body_idx] {
                     if body_literal == arg && matches!(body[body_idx - 1], Token::Dollar) {
-                        replace_map
-                            .entry(arg.clone())
-                            .or_insert_with(Vec::new)
-                            .push(body_idx);
+                        replace_map[arg_idx].push(body_idx);
                         break;
                     }
                 }
@@ -99,9 +97,20 @@ impl Expansion {
     }
 
     fn query(&self, query_tokens: Vec<Token>) -> Result<Vec<Token>, QueryError> {
-        let body_clone = self.body.clone();
+        let mut body_clone = self.body.clone();
         let mut idx = 0;
-
+        let mut literal_count = 0;
+        for t in query_tokens {
+            if matches!(t, Token::Literal(_)) { literal_count += 1; }
+        }
+        // go over all literals, make sure they have dollar before them, comma between args
+        // we are using only positional arguments here, first ensure that len of self.args and number of literals
+        // provided in query are same.
+        // for each Literal, have an index side by side for args and access that index in self.replace_map,
+        // in body_clone at the indices given by replace_map, replace that literal with whatever is in the query
+        if self.args.len() != literal_count - 1 {
+            return Err(QueryError::IncorrectNumberOfArguments);
+        }
         Ok(body_clone)
     }
 }
@@ -126,36 +135,36 @@ impl ExpansionEngine {
     pub fn query(&self, query_str: &str) -> Result<String, QueryError> {
         let mut ret_str = String::new();
         let query_tokens = tokenize(query_str);
-        let mut query_idx = 0;
-        while query_idx < query_tokens.len() {
-            if let Token::Literal(ref literal) = query_tokens[query_idx] {
+        let mut idx = 0;
+        while idx < query_tokens.len() {
+            if let Token::Literal(ref literal) = query_tokens[idx] {
                 if let Some(mac) = self.expansions.get(literal) {
                     let mut expansion_span = Vec::new();
-                    if matches!(query_tokens[query_idx+1], Token::Colon) &&
-                       matches!(query_tokens[query_idx+2], Token::OpenParentheses) 
+                    if matches!(query_tokens[idx+1], Token::Colon) &&
+                       matches!(query_tokens[idx+2], Token::OpenParentheses) 
                     { 
-                        query_idx += 3;
+                        idx += 3;
                     } else {
                         return Err(QueryError::MissingParentheses);
                     }
                     let mut span_marker = 1; 
-                    while query_idx < query_tokens.len() {
-                        match query_tokens[query_idx] {
+                    while idx < query_tokens.len() {
+                        match query_tokens[idx] {
                             Token::OpenParentheses => span_marker += 1,
                             Token::CloseParentheses => span_marker -= 1,
                             _ => {},
                         }
-                        expansion_span.push(query_tokens[query_idx].clone());
+                        expansion_span.push(query_tokens[idx].clone());
                         if span_marker == 0 { break; }
                     }
-                    let expansion_tokens = mac.query(expansion_span);
+                    let expansion_tokens = mac.query(expansion_span)?;
                     let expansion_str = expansion_tokens.iter().map(|t| t.as_str()).collect::<String>();
                     ret_str.push_str(&expansion_str);
                 }
             } else {
-                ret_str.push_str(query_tokens[query_idx].as_str());
+                ret_str.push_str(query_tokens[idx].as_str());
             }
-            query_idx += 1;
+            idx += 1;
         }
         Ok(ret_str)
     }
